5e6ea67fefeec4a59f004ae44d6b90b65b3c6b78
casterpunch <40738987+casterpunch@users.noreply.github.com>
2023-03-03 10:11:52 -0500

    Finished hooks setup

--- OP_APP/src/hooks.server.js
+++ OP_APP/src/hooks.server.js
@@ -7,5 +7,13 @@ export const handle = async ({event, resolve}) => {
 
     if (event.locals.pb.authStore.isValid) {
         event.locals.user = serializeNonPOJOs(event.locals.pb.authStore.model)
+    } else{
+        event.locals.user = undefined
     }
+
+    const response = await resolve(event)
+
+    response.headers.set('set-cookie', event.locals.pb.authStore.exportToCookie({secure:false}))
+
+    return response
 }
