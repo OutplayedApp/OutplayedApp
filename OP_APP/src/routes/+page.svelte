d7ccb9294d295a9710f702751b594fa6588c801a
Flynn McCallum <102677191+flagdanger@users.noreply.github.com>
2023-03-03 14:20:14 -0500

    header added to +page, can be moved to a separate file and imported

--- OP_APP/src/routes/+page.svelte
+++ OP_APP/src/routes/+page.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKiiiit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<header class="w-screen p-0 fixed flex items-center">
+    <div>
+        
+    </div>
+    <nav class="bg-slate-300 h-12 w-1/3">
+        <a
+        class="text-black text-sm uppercase hidden lg:inline-block font-semibold"
+        href="#pablo" on:click={(e) => e.preventDefault()}
+        >
+      Dashboard
+        </a>
+    </nav>
+    
+   
+    
+    <svg class=" fill-slate-300 h-12 w-auto " viewBox="0 0 2 3" aria-hidden="true">
+        <path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
+    </svg>
+
+    <svg class=" fill-slate-300 h-12 w-auto" viewBox="0 0 2 3" aria-hidden="true">
+        <path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
+    </svg>
+
+
+
+    <nav class="bg-slate-300 h-12 w-1/3">
+        <a
+        class="text-black text-sm uppercase hidden lg:inline-block font-semibold"
+        href="#pablo" on:click={(e) => e.preventDefault()}
+        >
+      Dashboard
+        </a>
+    </nav>
+    
+    
+
+
+
+</header>
