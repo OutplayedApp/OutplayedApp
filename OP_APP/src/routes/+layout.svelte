708936ce923fe83b4434d4af754553f0eb3146ff
Flynn McCallum <102677191+flagdanger@users.noreply.github.com>
2023-03-21 16:41:08 -0400

    dropdown menu
    slides open down from button

--- OP_APP/src/routes/+layout.svelte
+++ OP_APP/src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script>
 	import '../app.postcss';
+	import {slide} from "svelte/transition";
+	import { expoInOut } from 'svelte/easing'
 	let isMenuOpen = false;
   
 	function toggleMenu() {
@@ -12,21 +14,25 @@
   
 	<div class="flex relative">
 	  <button class="focus:outline-none" on:click={toggleMenu}>
-		<svg class="h-6 w-6 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+		<svg class=" rounded h-8 w-8 fill-current hover:bg-gray-600" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
 		  <rect x="4" y="5" width="16" height="2"/>
 		  <rect x="4" y="11" width="16" height="2"/>
 		  <rect x="4" y="17" width="16" height="2"/>
 		</svg>
 	  </button>
-	  <div class={isMenuOpen ? 'block absolute w-screen bg-gray-700 text-gray-100' : 'hidden'}>
+	</div>
+
+  </header>
+
+  	{#if isMenuOpen}
+	<div transition:slide class={isMenuOpen ? 'fixed top-12 right-0 w-32 p-2 bg-gray-700 text-gray-100' : 'hidden'}>
 		<a href="/profile" class="block px-4 py-2 hover:bg-gray-600">Profile</a>
 		<a href="/games" class="block px-4 py-2 hover:bg-gray-600">Games</a>
 		<a href="/account" class="block px-4 py-2 hover:bg-gray-600">Account</a>
 		<a href="/about" class="block px-4 py-2 hover:bg-gray-600">About</a>
 		<a href="https://github.com" class="block px-4 py-2 hover:bg-gray-600">GitHub</a>
-	  </div>
 	</div>
-  </header>
+	{/if}
 
   <slot></slot>
   
