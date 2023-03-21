9689383bcfa24c7c8f3ee74369e4449e47779ee6
Queenie Sun <91141179+honeyzuzu@users.noreply.github.com>
2023-03-21 17:35:18 -0400

    box style?

---
+++ OP_APP/src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Box from './Box.svelte';
+</script>
+
+<div class="p-20">
+	<h2>Hello!</h2>
+	<p>This is a box. It can contain anything.</p>
+</div>
---
+++ OP_APP/src/routes/Box.svelte
@@ -0,0 +1,14 @@
+<div class="box">
+	<slot></slot>
+</div>
+
+<style>
+	.box {
+		width: 300px;
+		border: 1px solid #aaa;
+        border-radius: 10px 140px 30px 140px;
+		box-shadow: 2px 2px 8px rgba(0,0,0,0.1);
+		padding: 1em;
+		margin: 0 0 1em 0;
+	}
+</style>
