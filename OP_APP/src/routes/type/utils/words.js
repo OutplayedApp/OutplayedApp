28d460d6a7ae7458568bdafbf783fcd862fb211a
Antonio Martinez III <toniom4172@gmail.com>
2024-01-28 22:55:31 -0500

    FRESH START RAHHH

--- OP_APP/src/routes/type/utils/words.js
+++
@@ -1,5 +0,0 @@
-export default [
-    'apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape', 'honeydew', 'indigo', 'jackfruit',
-    'kiwi', 'lemon', 'mango', 'nectarine', 'orange', 'pear', 'quince', 'raspberry', 'strawberry', 'tangerine',
-    'watermelon', 'xylophone', 'yellow', 'zebra'
-  ];
