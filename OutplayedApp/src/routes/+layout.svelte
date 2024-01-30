3472c997576e546854287c8ae8e078cf1790df22
Antonio Martinez III <toniom4172@gmail.com>
2024-01-30 16:36:37 -0500

    working nav bar

--- OutplayedApp/src/routes/+layout.svelte
+++ OutplayedApp/src/routes/+layout.svelte
@@ -2,10 +2,40 @@
 	import '../app.pcss';
 	import Header from './Header.svelte';
 	import './styles.css';
+	import Logo from '../lib/images/Logo.svg';
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Avatar, Dropdown, DropdownItem, DropdownHeader, DropdownDivider } from 'flowbite-svelte';
 </script>
 
 <div class="app">
-	<Header></Header>
+	<!-- <Header></Header> -->
+	  
+	<Navbar>
+		<NavBrand href="/">
+			<img src={Logo} class="me-3 h-6 sm:h-9" alt="Outplyed Logo" />
+			<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Outplayed</span>
+		</NavBrand>
+		<NavUl>
+			<NavLi href="/" active={true}>Home</NavLi>
+			<NavLi href="/games">Games</NavLi>
+			<NavLi href="/leaders">Leaderboards</NavLi>
+			<NavLi href="/stats">Stats</NavLi>
+		</NavUl>
+		<div class="flex items-center md:order-2">
+			<Avatar id="avatar-menu" src="https://i.stack.imgur.com/frlIf.png" />
+			<NavHamburger class1="w-full md:flex md:w-auto md:order-1" />
+		</div>
+		<Dropdown placement="bottom" triggeredBy="#avatar-menu">
+			<DropdownHeader>
+			<span class="block text-sm">Antonio Martinez III</span>
+			<span class="block truncate text-sm font-medium">#1 EFT Player NA</span>
+			</DropdownHeader>
+			<DropdownItem>Profile</DropdownItem>
+			<DropdownItem>Settings</DropdownItem>
+			<DropdownItem>Some Tab</DropdownItem>
+			<DropdownDivider />
+			<DropdownItem>Sign out</DropdownItem>
+		</Dropdown>
+	</Navbar>
 
 	<main>
 		<slot />
