d5cb6148f664d277cf2a2da5e6f8c52fe7e1dbbe
Antonio Martinez III <toniom4172@gmail.com>
2024-01-29 20:36:42 -0500

    start nav bar

---
+++ OutplayedApp/src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script>
+    import "../app.css";
+    import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Avatar, Dropdown, DropdownItem, DropdownHeader, DropdownDivider } from 'flowbite-svelte';
+    import Logo from "$lib/assets/logo.svg";
+</script>
+
+<div class = "container">
+    <!-- Navbar -->
+    <NavBrand href = "/">
+        <!-- display logo.svg from the images directory-->
+        <img src={Logo} class="me-3 h-6 sm:h-9" alt="Outplayed" width = "100" height = "100">
+        <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-black light:text-white">Outplayed</span>
+    </NavBrand>
+    <!-- Navbar END-->
+
+    <!-- Content -->
+    <slot></slot>
+    <!-- Content END-->
+
+    <!-- Footer -->
+    <!-- Footer END-->
+</div>
