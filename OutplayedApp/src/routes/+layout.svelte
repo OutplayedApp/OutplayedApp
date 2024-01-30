912550d81c67b1e23b110f3faca71f213ae07742
Antonio Martinez III <toniom4172@gmail.com>
2024-01-30 16:19:19 -0500

    nuked lol

--- OutplayedApp/src/routes/+layout.svelte
+++ OutplayedApp/src/routes/+layout.svelte
@@ -1,45 +1,53 @@
 <script>
-    import "../app.css";
-    import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Avatar, Dropdown, DropdownItem, DropdownHeader, DropdownDivider } from 'flowbite-svelte';
-    import Logo from "$lib/assets/logo.svg";
+	import Header from './Header.svelte';
+	import './styles.css';
 </script>
 
-<div class = "container">
-    <!-- Navbar -->
-    <Navbar rounded color="form">
-        <NavBrand href = "/">
-            <!-- display logo.svg from the images directory-->
-            <img src={Logo} class="me-3 h-6 sm:h-9" alt="Outplayed" width = "100" height = "100">
-            <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-black light:text-white">Outplayed</span>
-        </NavBrand>
-        <div class="flex items-center md:order-2">
-            <Avatar id="avatar-menu" src="https://avatars.githubusercontent.com/u/44908413?v=4"/>
-            <NavHamburger class1="w-full md:flex md:w-auto md:order-1"/>
-        </div>
-        <Dropdown placement="bottom" triggeredBy="#avatar-menu">
-            <DropdownHeader>
-                <span class="block text-sm">Antonio Martinez III</span>
-                <span class="block truncate text-sm font-medium">#1 EFT Player</span>
-            </DropdownHeader>
-            <DropdownItem>Profile</DropdownItem>
-            <DropdownItem>Settings</DropdownItem>
-            <DropdownItem>Some Tab</DropdownItem>
-            <DropdownDivider />
-            <DropdownItem>Logout</DropdownItem>
-        </Dropdown>
-        <NavUl>
-            <NavLi href = "/">Home</NavLi>
-            <NavLi href = "/games">Games</NavLi>
-            <NavLi href = "/leaders">Leaderboards</NavLi>
-            <NavLi href = "/profile">Profile</NavLi>
-        </NavUl>
-    </Navbar>
-    <!-- Navbar END-->
+<div class="app">
+	<Header />
 
-    <!-- Content -->
-    <slot></slot>
-    <!-- Content END-->
+	<main>
+		<slot />
+	</main>
 
-    <!-- Footer -->
-    <!-- Footer END-->
+	<footer>
+		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+	</footer>
 </div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+</style>
